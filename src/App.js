438a25db27d02290c6bc08caea071f36dd504401
Ryan Wang <hwwangwang@gmail.com>
2017-11-29 16:13:36 +0800

    setup react env

---
+++ src/App.js
@@ -0,0 +1,5 @@
+
+import React from 'react'
+
+
+export default () => <div>react hello</div>;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+
+      }
+    ]
+  }
+
+}
+
