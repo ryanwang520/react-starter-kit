245a2788eff502c23e1e1a3bed0b92f0af0ac72b
Ryan Wang <hwwangwang@gmail.com>
2017-11-29 23:18:01 +0800

    common chunk plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import _ from 'lodash'
+_.join(['a', 'b'])
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
@@ -30,6 +31,9 @@ module.exports = {
       title: 'Output Management',
       template: 'assets/index.html',
 
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
     })
   ]
 
