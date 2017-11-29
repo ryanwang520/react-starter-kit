61704317575e208ccac9f62d354ce4b227c1e825
Ryan Wang <hwwangwang@gmail.com>
2017-11-29 20:36:02 +0800

    asset manage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 
-import React from 'react'
+import React, { Fragment } from 'react'
+import './style.css'
+import Icon from './icon.png'
 
 
-export default () => <div>react hello</div>;
+export default () => <Fragment>
+  <div class="hello">react hello</div>
+  <img src={Icon} alt="icon" />
+  </Fragment>;
--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,36 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }, {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+
+      }, {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
 
       }
+
     ]
   }
 
