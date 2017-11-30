b96f7bc31353e0d6c678638fa749145b429148af
Ryan Wang <hwwangwang@gmail.com>
2017-11-30 14:42:01 +0800

    use dll

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
-const path = require('path')
-
-module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-
-      }
-    ]
-  }
-
-}
-
+var path = require("path");
+var webpack = require("webpack");
+module.exports = [
+	{
+		name: "vendor",
+		entry: ["./vendor", "./vendor2", "react", "react-dom"],
+		output: {
+			path: path.resolve(__dirname, "js"),
+			filename: "vendor.js",
+			library: "vendor_[hash]"
+		},
+		plugins: [
+			new webpack.DllPlugin({
+				name: "vendor_[hash]",
+				path: path.resolve(__dirname, "js/manifest.json")
+			})
+		]
+	},
+	{
+		name: "app",
+		dependencies: ["vendor"],
+		entry: {
+			pageA: "./pageA",
+			pageB: "./pageB",
+			pageC: "./pageC"
+		},
+		output: {
+			path: path.join(__dirname, "js"),
+			filename: "[name].js"
+		},
+		plugins: [
+			new webpack.DllReferencePlugin({
+				manifest: path.resolve(__dirname, "js/manifest.json")
+			})
+		]
+	}
+];
