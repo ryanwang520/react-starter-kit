dc6d59793b9470539bcafcf96a972a7875f8cae4
Ryan Wang <hwwangwang@gmail.com>
2017-11-30 12:26:12 +0800

    set webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
 const path = require('path')
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
+module.exports = env => {
+  console.log(env)
+  return {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
 
-      }
-    ]
+        }
+      ]
+    }
   }
 
 }
