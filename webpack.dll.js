b925c08ad051a8f8462de2c849fde330f7001c8d
Ryan Wang <hwwangwang@gmail.com>
2017-12-10 03:56:49 +0800

    not hard code dll

--- webpack.dll.js
+++ webpack.dll.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const pkg = require(path.join(process.cwd(), 'package.json'))
+
 module.exports = {
   name: 'vendor',
   entry: {
-    vendor: ['react', 'react-dom', 'babel-polyfill', 'react-hot-loader'],
+    vendor: Object.keys(pkg.dependencies),
   },
   output: {
     path: path.resolve(__dirname, 'dll'),
