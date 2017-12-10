29f95f2faacd7a41aa6e7abb6ebb930eac5fed4c
Ryan Wang <hwwangwang@gmail.com>
2017-12-11 00:09:21 +0800

    the router key hack is not correct

--- src/App.js
+++ src/App.js
@@ -3,17 +3,8 @@ import Router from './Router'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles'
 
-let App
-if (module.hot) {
-  // dirty hack base on https://medium.com/@giang.nguyen.dev/hot-loader-with-react-loadable-c8f70c8ce1a6
-  // force trigger a render on route
-  App = () => <Router key={Math.random()} />
-} else {
-  App = () => <Router />
-}
-
 export default () => (
   <ThemeProvider theme={theme}>
-    <App />
+    <Router />
   </ThemeProvider>
 )
