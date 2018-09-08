a850a6109deefeb86d19cf79d65af58c3cc3144e
Ryan Wang <hwwangwang@gmail.com>
2018-09-08 11:19:01 +0800

    preset use usage

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Production',
