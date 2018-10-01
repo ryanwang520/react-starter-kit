173e50616e4675e174ddc8b93e4cfb46e0fed70e
Ryan Wang <hwwangwang@gmail.com>
2018-10-01 12:26:50 +0800

    tweak hot loader

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import App from './App'
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root')
-  )
-}
-
-render(App)
+ReactDOM.render(<App />, document.getElementById('root'))
