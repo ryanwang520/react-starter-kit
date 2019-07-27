88e4b7a5569797821e104d5c63aa7f05e751b30b
Ryan Wang <hwwangwang@gmail.com>
2019-07-27 21:50:42 +0800

    bump dependency

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 // @flow
-import React, { Fragment } from 'react'
-import Loadable from 'react-loadable'
+import React, { lazy, Suspense } from 'react'
 
 import Header from './Containers/Header'
 
@@ -15,28 +14,20 @@ import {
 import styled from 'styled-components'
 import auth from './auth'
 
-const DefaultLoadingIndicator = () => null
-
 const StyledLink = styled(Link)`
   padding: 10px;
 `
 
-const AsyncLoading = (imported, LoadingIndicator = DefaultLoadingIndicator) =>
-  Loadable({
-    loader: imported,
-    loading: LoadingIndicator,
-  })
-
-const Home = AsyncLoading(() =>
+const Home = lazy(() =>
   import(/* webpackChunkName: "Home" */ './Containers/Home')
 )
-const About = AsyncLoading(() =>
+const About = lazy(() =>
   import(/* webpackChunkName: "About" */ './Containers/About')
 )
-const Trend = AsyncLoading(() =>
+const Trend = lazy(() =>
   import(/* webpackChunkName: "Trend" */ './Containers/Trend')
 )
-const Login = AsyncLoading(() =>
+const Login = lazy(() =>
   import(/* webpackChunkName: "Login" */ './Containers/Login')
 )
 
@@ -79,19 +70,21 @@ const AuthButton = withRouter(Button)
 
 export default () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Fragment>
-        <AuthButton />
-        <Header>
-          <StyledLink to="/">home</StyledLink>
-          <StyledLink to="/about">about</StyledLink>
-          <StyledLink to="/trend">trend</StyledLink>
-        </Header>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <PrivateRoute path="/trend" component={Trend} />
-      </Fragment>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <>
+          <AuthButton />
+          <Header>
+            <StyledLink to="/">home</StyledLink>
+            <StyledLink to="/about">about</StyledLink>
+            <StyledLink to="/trend">trend</StyledLink>
+          </Header>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <PrivateRoute path="/trend" component={Trend} />
+        </>
+      </Switch>
+    </Suspense>
   </BrowserRouter>
 )
