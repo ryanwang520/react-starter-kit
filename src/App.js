8f1b7dfd3f52c3c575108f70acf653e9a298662b
Ryan Wang <hwwangwang@gmail.com>
2019-11-01 17:20:53 +0800

    remove flow styled-components helmet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-// @flow
 import React from 'react'
 import Router from './Router'
-import { ThemeProvider } from 'styled-components'
-import { theme } from './styles'
 import { hot } from 'react-hot-loader/root'
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router />
-  </ThemeProvider>
-)
+const App = () => <Router />
 export default hot(App)
